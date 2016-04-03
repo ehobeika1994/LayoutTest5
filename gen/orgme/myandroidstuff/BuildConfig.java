9811dd6c095bd8c5f07643b904b8de7ac9f9694d
Edmond Hobeika <eddiehobeika1994@hotmail.com>
2016-04-03 20:39:19 +0100

    Layout Test 5 - Android

---
+++ gen/orgme/myandroidstuff/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package orgme.myandroidstuff;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/orgme/myandroidstuff/LayoutTest5Activity.java
@@ -0,0 +1,13 @@
+package orgme.myandroidstuff;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LayoutTest5Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
